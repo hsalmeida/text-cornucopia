61e1225b60d03411ae1ef8c368cf1f9af574181d
hsalmeida <hsalmeida@gmail.com>
2015-11-26 17:27:35 -0200

    beta test

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ var cornucopia = angular.module('cornucopia', [])
         $scope.lines = 0;
         $scope.lineSeparator = "\\n";
         $scope.separator = ",";
-        $scope.panel = "";
-        $scope.pattern = "";
+        $scope.panel = "Lumbergh, Bill, Initech\r\nWaddams, Milton, Initech\r\nGibbons, Peter, Initech\r\nBolton, Michael, Initech";
+        $scope.pattern = "To: $1.$0@$2.com\nHello $1 $0,\nI'm sorry to inform you of a terrible accident at $2.\n---";
         $scope.results = "";
 
         function interpret(input) {
@@ -13,21 +13,24 @@ var cornucopia = angular.module('cornucopia', [])
         }
 
         $scope.execute = function(){
-            console.log($scope.pattern);
-            console.log($scope.separator);
-            console.log($scope.results);
 
             var ls = interpret($scope.lineSeparator);
             var lines = $scope.panel.split(ls);
             var lenLines = lines.length;
+            $scope.lines = lenLines;
+            var result = "";
             lines.forEach(function(line){
                 var elements = line.split($scope.separator);
-                console.log(elements);
-
-                var pat = $scope.pattern;
-                pat.replace();
-
+                var copyPattern = String($scope.pattern);
+                for(var i in elements) {
+                    var repStr = new String('\\$' + i);
+                    var re = new RegExp(repStr, 'gim');
+                    copyPattern = copyPattern.replace(re, elements[i].trim());
+                }
+                copyPattern += "\r\n";
+                result += copyPattern;
             });
 
+            $scope.results = result;
         };
 }]);
