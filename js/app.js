b33df757fa588e294b6d6118504afbe2ffed7c66
hsalmeida <hsalmeida@gmail.com>
2015-11-27 11:10:48 -0200

    beta update

--- js/app.js
+++ js/app.js
@@ -3,21 +3,34 @@ var cornucopia = angular.module('cornucopia', [])
         $scope.lines = 0;
         $scope.lineSeparator = "\\n";
         $scope.separator = ",";
-        $scope.panel = "Lumbergh, Bill, Initech\r\nWaddams, Milton, Initech\r\nGibbons, Peter, Initech\r\nBolton, Michael, Initech";
-        $scope.pattern = "To: $1.$0@$2.com\nHello $1 $0,\nI'm sorry to inform you of a terrible accident at $2.\n---";
+        $scope.panel = "Name, Bill, 10\r\nName, Milton, 11\r\nLastName, Peter, 12\r\nName, Michael, 13";
+        $scope.pattern = "UPDATE User SET $0 = '$1' WHERE UserId = $2";
         $scope.results = "";
+        $scope.chAuto = false;
+        $scope.panelLines = 0;
+        $scope.patternLines = 0;
+        var lineSep = interpret($scope.lineSeparator);
+        $scope.panelLines = $scope.panel.split(lineSep).length;
+        $scope.patternLines = $scope.pattern.split(lineSep).length;
 
         function interpret(input) {
             //convert the literal string '\r' into a carriage return (\r). ditto for '\n' and '\t'
             return input.replace(/\\r/g, '\r').replace(/\\n/g, '\n').replace(/\\t/g, '\t');
         }
 
-        $scope.execute = function(){
+        $scope.auto = function(){
+            if($scope.chAuto && $scope.panel.length < 10000) {
+                $scope.execute();
+            }
+            var ls = interpret($scope.lineSeparator);
+            $scope.panelLines = $scope.panel.split(ls).length;
+            $scope.patternLines = $scope.pattern.split(ls).length;
+        };
 
+        $scope.execute = function(){
             var ls = interpret($scope.lineSeparator);
             var lines = $scope.panel.split(ls);
-            var lenLines = lines.length;
-            $scope.lines = lenLines;
+            $scope.lines = lines.length;
             var result = "";
             lines.forEach(function(line){
                 var elements = line.split($scope.separator);
